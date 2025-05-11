e78487149d285e53c9f5cc7fb16eeafceea3cebe
eleminants <purnendusarangi797@gmail.com>
2025-05-12 00:33:21 +0530

    Changes done on 12-05-2025

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  form: any; // Declare the form property here
 
+  constructor(public formBuilder: FormBuilder) { // Initialize formBuilder in the constructor
+    this.form = this.formBuilder.group({
+      fullName : [''],
+      email: [''],
+      password: [''],
+      confirmPassword: ['']
+    });
+  }
+  onSubmit(){
+    console.log(this.form.value);
+   } 
 }
