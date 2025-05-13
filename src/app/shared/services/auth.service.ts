456c824e17e3edc9ed1bb504463d81d190c58993
eleminants <purnendusarangi797@gmail.com>
2025-05-13 19:13:42 +0530

    changes done on 13-05-2025

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  baseUrl = 'https://localhost:7130/api';
+  createUser(formData: any){debugger;
+    return this.http.post(this.baseUrl+'/signup', formData);
+  }
 }
--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,24 +1,62 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { FirstKeyPipe } from '../../shared/pipes/first-key.pipe';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-registration',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, FirstKeyPipe],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
   form: any; // Declare the form property here
-
-  constructor(public formBuilder: FormBuilder) { // Initialize formBuilder in the constructor
-    this.form = this.formBuilder.group({
-      fullName : [''],
-      email: [''],
-      password: [''],
-      confirmPassword: ['']
-    });
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+    const password = control.get('password')
+    const confirmPassword = control.get('confirmPassword')
+    if (password && confirmPassword && password.value != confirmPassword.value)
+      confirmPassword?.setErrors({ passwordMismatch: true })
+    else
+      confirmPassword?.setErrors(null)
+    return null;
   }
-  onSubmit(){
-    console.log(this.form.value);
-   } 
+  isSubmitted: boolean = false;
+
+  constructor(public formBuilder: FormBuilder, private service: AuthService) { // Initialize formBuilder in the constructor
+
+    this.form = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/)]],
+      confirmPassword: ['']
+    }, { validators: this.passwordMatchValidator });
+  }
+  onSubmit() {
+    this.isSubmitted = true
+    if (this.form.valid) {debugger;
+      this.service.createUser(this.form.value)
+    .subscribe({
+        next: (res:any) => {
+          if(res.succeeded){
+            this.form.reset();
+            this.isSubmitted = false;
+            
+          }
+          else{
+            console.log('response', res);
+          }
+          
+
+        },
+        error: err => console.log('error', err)
+      });
+    }
+  }
+
+  hasDisplaybleError(controlName: string): Boolean {
+    const control = this.form.get(controlName);
+    return Boolean(control?.invalid) && (this.isSubmitted || Boolean(control?.touched))
+
+  }
 }
