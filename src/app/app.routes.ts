e5f4dbd15d6468018911bc7aec9d5e987e9fd3d2
eleminants <purnendusarangi797@gmail.com>
2025-05-15 23:31:24 +0530

    Changes done on 15-05-2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'user',component:UserComponent,
+        children:[
+            {path: 'signup',component:RegistrationComponent}
+        ]
+    }
+];
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RegistrationComponent } from './registration/registration.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [RegistrationComponent],
+  imports: [RegistrationComponent,RouterOutlet],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
